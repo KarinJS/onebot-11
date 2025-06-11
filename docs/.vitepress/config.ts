ccbedcab497bfe74149e51802836f06e8201007a
时瑾 <74231782+sj817@users.noreply.github.com>
2025-06-11 18:35:41 +0800

    fix: base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/onebot-11/',
+  base: '/',
   title: "onebot11",
   description: "太懒了 还没有",
   vite: {
