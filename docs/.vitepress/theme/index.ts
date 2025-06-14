16c75db92702e134802838163dfa130f17c67e73
炫炫 <2066855608@qq.com>
2025-06-15 04:11:18 +0800

    类型

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,18 @@
 import DefaultTheme from 'vitepress/theme'
+import type { App } from 'vue'
 import './style/custom-block.css'
 import LlmsButton from './components/LlmsButton.vue'
+import HighPerformanceTable from './components/HighPerformanceTable.vue'
 import Layout from './Layout.vue'
 
 export default {
   extends: DefaultTheme,
   Layout,
-  enhanceApp({ app }) {
+  enhanceApp({ app }: { app: App }) {
     app.component('LlmsButton', LlmsButton)
+    app.component('HighPerformanceTable', HighPerformanceTable)
   },
   setup() {
     // 这里可以添加全局的setup逻辑
   }
-} 
+}
